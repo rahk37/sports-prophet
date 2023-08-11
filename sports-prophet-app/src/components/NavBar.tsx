a34d98b37cb5e0ebe6047e07aee01feaf7e04f11
Wikel <riley.wikel@cognizant.com>
2023-08-11 13:42:14 -0500

    Adding mobile navbar

--- sports-prophet-app/src/components/NavBar.tsx
+++ sports-prophet-app/src/components/NavBar.tsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, {useState} from "react";
+import "../styles/components/NavBar.scss";
 
 function NavBar() {
+    const [isNavOpen, setOpenNav] = useState(false);
+
+    const openNav = () => {
+        setOpenNav(!isNavOpen);
+    };
+
   return (
-    <div>
-      hello
-    </div>
+    <nav className={`navbar ${isNavOpen ? 'open' : ''}`}>
+      <div className="logo">SportsProphet</div>
+
+      <button className="nav-btn" onClick={openNav}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </button>
+
+      <ul className={`nav-links ${isNavOpen ? 'open' : ''}`}>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/football">Football</a>
+        </li>
+        <li>
+          <a href="/baskeball">Basketball</a>
+        </li>
+        <li>
+          <a href="/hockey">Hockey</a>
+        </li>
+        <li>
+          <a href="/ufc">UFC</a>
+        </li>
+      </ul>
+    </nav>
   );
 }
 

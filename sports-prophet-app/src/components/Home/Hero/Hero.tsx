b02cf0d1cce42a43c558d1abe1e85721c4c1b3ef
Wikel <riley.wikel@cognizant.com>
2023-08-16 15:25:43 -0500

    Adding more style to hero component

--- sports-prophet-app/src/components/Home/Hero/Hero.tsx
+++ sports-prophet-app/src/components/Home/Hero/Hero.tsx
@@ -2,14 +2,68 @@ import React from "react";
 import "../../../styles/components/Home/Hero/Hero.scss";
 
 function Hero() {
-
   return (
     <div className="hero">
-        <div className="sports-field">
-            <div className="field-line"></div>
-            <div className="basketball"></div>
+
+      <div className="mountain">
+        <div className="mountain-top">
+          <div className="mountain-cap-1"></div>
+          <div className="mountain-cap-2"></div>
+          <div className="mountain-cap-3"></div>
         </div>
-        <div className="mountains"></div>
+      </div>
+      <div className="mountain-two">
+        <div className="mountain-top">
+          <div className="mountain-cap-1"></div>
+          <div className="mountain-cap-2"></div>
+          <div className="mountain-cap-3"></div>
+        </div>
+      </div>
+      <div className="mountain-three">
+        <div className="mountain-top">
+          <div className="mountain-cap-1"></div>
+          <div className="mountain-cap-2"></div>
+          <div className="mountain-cap-3"></div>
+        </div>
+      </div>
+      <div className="cloud"></div>
+
+      <div className="mountain2">
+        <div className="mountain-top2">
+          <div className="mountain-cap-12"></div>
+          <div className="mountain-cap-22"></div>
+          <div className="mountain-cap-32"></div>
+        </div>
+      </div>
+      <div className="mountain-two2">
+        <div className="mountain-top2">
+          <div className="mountain-cap-12"></div>
+          <div className="mountain-cap-22"></div>
+          <div className="mountain-cap-32"></div>
+        </div>
+      </div>
+      <div className="mountain-three2">
+        <div className="mountain-top2">
+          <div className="mountain-cap-12"></div>
+          <div className="mountain-cap-22"></div>
+          <div className="mountain-cap-32"></div>
+        </div>
+      </div>
+      <div className="cloud2"></div>
+
+      <div className="hero-content">
+        <h1 className="hero-header">Imagine a Magic Crystal...</h1>
+        <p className="hero-p">
+          ...that predicts professional sports outcomes with remarkable
+          precision. Witness predictions materialize with a simple tap. What's
+          so enchanting? It's absolutely free, making predicting game outcomes a
+          breeze.
+        </p>
+        <div className="hero-btns">
+          <button className="btn btn-hero-secondary">Get Started</button>
+          <button className="btn btn-hero-primary">Predict Now</button>
+        </div>
+      </div>
     </div>
   );
 }
